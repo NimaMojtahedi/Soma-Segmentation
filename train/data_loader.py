8f962057e8a76939caa3c93482739d49acdc46d5
Nima Mojtahedi <nima.mojtahedigm@gmail.com>
2021-10-06 15:09:39 +0200

    add detectron2 data loader as data_loader.py

---
+++ train/data_loader.py
@@ -0,0 +1,21 @@
+# In detectron2 to load data from json format we need to register it in Detectron2 dataset catalog.
+
+# import librairies
+import json
+from detectron2.data import MetadataCatalog, DatasetCatalog
+
+
+# function to load json file
+def load_dataset(data_path):
+
+    with open(data_path, 'r') as f:
+        dataset = json.load(f)
+
+
+# register and define classes
+def register(dataset, classes, dataset_name="train"):
+
+    DatasetCatalog.register(dataset_name, dataset)
+    MetadataCatalog.get(dataset_name).set(thing_classes=classes)
+
+    return MetadataCatalog.get(dataset_name)
